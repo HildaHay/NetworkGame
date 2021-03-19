213e4a09d60678012f4107641efe88e2c3c997af
Hilda <eskarina@eskarina.ca>
2021-03-18 20:07:32 -0700

    Added bullets

--- Assets/script/PlayerMoveScript.cs
+++ Assets/script/PlayerMoveScript.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerMoveScript : MonoBehaviour
+public class PlayerMoveScript : NetworkBehaviour
 {
     NetworkIdentity networkIdentity;
 
     private bool facing_left = true;
     private SpriteRenderer m_sprite;
+
+    public GameObject bullet;
+
+    Vector2Int direction = new Vector2Int( 1, 0 );
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +24,9 @@ public class PlayerMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        direction.x = 0;
+        direction.y = 0;
+
         if (m_sprite == null)
         {
             fetchSpriteRenderer();
@@ -28,20 +36,29 @@ public class PlayerMoveScript : MonoBehaviour
             if (Input.GetKey("w"))
             {
                 this.transform.position += new Vector3(0.0f, 1.0f, 0.0f) * Time.deltaTime;
+                direction.y += 1;
             }
             if (Input.GetKey("a"))
             {
                 facing_left = true;
                 this.transform.position += new Vector3(-1.0f, 0.0f, 0.0f) * Time.deltaTime;
+                direction.x -= 1;
             }
             if (Input.GetKey("s"))
             {
                 this.transform.position += new Vector3(0.0f, -1.0f, 0.0f) * Time.deltaTime;
+                direction.y -= 1;
             }
             if (Input.GetKey("d"))
             {
                 facing_left = false;
                 this.transform.position += new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime;
+                direction.x += 1;
+            }
+
+            if (Input.GetKeyDown("space"))
+            {
+                CmdFireBullet(direction.x, direction.y);
             }
         }
 
@@ -51,5 +68,14 @@ public class PlayerMoveScript : MonoBehaviour
     void fetchSpriteRenderer()
     {
         m_sprite = GetComponent<SpriteRenderer>();
+    }
+
+    [Command]
+    void CmdFireBullet(int x, int y)
+    {
+        Debug.Log("shoot");
+        GameObject newBullet = Instantiate(bullet, this.transform.position, Quaternion.identity);
+        newBullet.GetComponent<Rigidbody2D>().velocity = new Vector3(x * 10, y * 10, 0);
+        NetworkServer.Spawn(newBullet);
     }
 }
